bfbc0ad6c59ba9e61dabb9771c19991877fa055b
AberQ <egor.master2017@gmail.com>
2025-02-14 18:55:09 +0500

    Добавил модель для товаров

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Merch(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.name} - {self.price}"
